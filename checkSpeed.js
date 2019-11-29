29d196abd1f9ba0b96d7365f850e8b5fbfeef3e1
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-11-28 20:30:29 -0800

    Update checkSpeed.js

--- checkSpeed.js
+++ checkSpeed.js
@@ -1,3 +1,13 @@
+/* 
+Checks the speed.
+
+If the speed is under 75 mph, everything is ok.
+If the speed is equal to or above 75 mph, they get a point for every 5 miles that they are over.
+Math.floor is used to round down floats. 
+If they get 12 points they have a suspended license. 
+*/
+
+
 function checkSpeed(speed) {
     const speedLimit = 70;
     const perPoint = 5;
