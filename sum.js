dcea0cb18c5a7d053af98f66107b85f66c9bdbc4
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-11-29 12:58:33 -0800

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,16 @@
+/*
+A function that adds up the total for
+multiples of 5 and 3.
+
+Number limit is set within the function.
+*/
+
+function sum(limit) {
+    let total = 0;
+    for (let i = 1; i <=10; i++)
+        if (i % 5 === 0 || i % 3 === 0)
+            total += i;
+            return total;
+}
+
+console.log(sum(10));
