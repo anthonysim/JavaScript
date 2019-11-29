973a7e1ea9f39d99e50cedf398de36b29b6cd887
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-11-29 14:49:06 -0800

    Add files via upload

---
+++ showStars.js
@@ -0,0 +1,16 @@
+/*
+A function that forms a "tree" based on
+the amount of rows specified.
+*/
+
+
+function showStars(row) {
+    let stars = ''
+
+    for (let i = 0; i < row; i++) {     
+        stars += '*';
+        console.log(stars);
+    }
+}
+
+showStars(10);
