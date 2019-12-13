b2561856146d58a4fa32e7cc82a5ea2b5275fa8d
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-12-12 21:13:16 -0800

    Update basketball.js

--- Complete_JavaScript_Course_2020/basketball.js
+++ Complete_JavaScript_Course_2020/basketball.js
@@ -23,12 +23,12 @@ console.log(johnAvg, mikeAvg, maryAvg);
 if (johnAvg > mikeAvg && johnAvg > maryAvg) {
     console.log('The winner is JOHN with an average score of: ' + johnAvg);
 
-} else if (mikeAvg > johnAvg && mikeAvg> maryAvg) {
+} else if (mikeAvg > johnAvg && mikeAvg > maryAvg) {
     console.log('The winner is MIKE with an average score of: ' + mikeAvg);
 
 } else if (maryAvg > mikeAvg && maryAvg > johnAvg) {
     console.log('The winner is MARY with an average score of: ' + maryAvg);
-    
+
 } else {
     console.log('The average scores were a draw!');
 };
