fbf92136a87fb98ad766bd24529eba48f3413ba9
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-12-12 19:46:13 -0800

    Add files via upload

---
+++ age.js
@@ -0,0 +1,12 @@
+let firstName = 'John';
+let age = 13;
+
+if (age < 13) {
+    console.log(firstName + ' is a a boy!');
+} else if (age < 20) {
+    console.log(firstName + ' is a a teenager!');
+} else if (age < 30) {
+    console.log(firstName + ' is a a young man!');
+} else {
+    console.log(firstName + ' is a man!')
+}
