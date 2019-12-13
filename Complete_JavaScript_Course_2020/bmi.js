1b179590b46613e29bc62cbd5a2eba3819cc5408
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-12-12 20:30:08 -0800

    Create bmi.js

---
+++ Complete_JavaScript_Course_2020/bmi.js
@@ -0,0 +1,18 @@
+/*
+Coding Challenge 1
+
+Calculated the bmi for mark and john, compares the both person's bmis against each other
+to determine is mark has a higher bmi than john.
+*/
+
+let BMI, mass, height, mark, john;
+
+BMI = (mass, height) => (mass / (height ** 2 ));
+
+mark = BMI(78, 1.69);
+john = BMI(92, 1.95);
+console.log(mark, john);
+
+let versus = Boolean(mark > john)
+
+console.log("Is Mark's BMI higher than Johns?", versus);
