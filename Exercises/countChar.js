583fddfd9dd3eb728d162f36c0baedb70d23bb46
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-12-24 12:22:40 -0800

    Create countChar.js

---
+++ Exercises/countChar.js
@@ -0,0 +1,26 @@
+/*
+2 functions:
+1) counts the number of letters in a given word.
+2) counts the number of "Bs" in a word.
+*/
+
+
+function countChar(word, letter) {
+    word = word.toUpperCase();
+    letter = letter.toUpperCase();
+
+    let count = 0;
+    for ( char of word ) {
+        if ( char === letter ) 
+            count += 1;
+    }
+    return count;
+}
+
+console.log(countChar('kakkerlak','k'));
+
+function countB(string) {
+    return countChar(string, 'B');
+}
+
+console.log(countBs('BBC'));
