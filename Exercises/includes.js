c5fe30265555883aa5af38384fffcb786fe75a39
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-07-18 00:07:07 -0700

    Update includes.js

--- Exercises/includes.js
+++ Exercises/includes.js
@@ -2,13 +2,10 @@
 A function that checks to see if a a number is inside an array.
 */
 
-const numbers = [1, 2, 3, 4]
 
-function includes(array, searchElement) {
-    for (let i = 0; i < numbers.length; i++)
-        if (searchElement === numbers[i])
-        return true
-    return false
+function includes(arr, searchElement) {
+   return arr.some(num => (searchElement === num) ? true : false)
 }
 
+const numbers = [1, 2, 3, 4]
 console.log(includes(numbers, 1));
