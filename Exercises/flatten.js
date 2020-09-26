13347e0d1315521095ce16fa57b0b8d1e3dd78b2
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-09-26 12:40:57 -0700

    Update flatten.js

--- Exercises/flatten.js
+++ Exercises/flatten.js
@@ -12,3 +12,9 @@ function flatten(oldArr){
   
   return newArr;
 }
+
+
+console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
