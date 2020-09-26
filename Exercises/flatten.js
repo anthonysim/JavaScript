ec5caf07ce2e7bb2950039c24a6c23fda366f7eb
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-09-26 12:41:08 -0700

    Update flatten.js

--- Exercises/flatten.js
+++ Exercises/flatten.js
@@ -17,4 +17,4 @@ function flatten(oldArr){
 console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
 console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
 console.log(flatten([[1],[2],[3]])) // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
