51db3a06d67fd1a68bfe0f2fa7fb0ef19819b279
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-09-26 12:39:38 -0700

    Create flatten.js

---
+++ Exercises/flatten.js
@@ -0,0 +1,14 @@
+function flatten(oldArr){
+  let newArr = [];
+  
+  	for (var i = 0; i < oldArr.length; i++) {
+    	if(Array.isArray(oldArr[i])){
+      		newArr = newArr.concat(flatten(oldArr[i]));
+          
+    	} else {
+      		newArr.push(oldArr[i]);
+    	}
+  } 
+  
+  return newArr;
+}
